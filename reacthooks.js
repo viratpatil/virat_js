4c0bd2b93449ab5b0e90f2c591ec2513698b2f90
virat <viratpatil@gmail.com>
2024-01-17 23:34:40 +0000

    useContext

--- reacthooks.js
+++ reacthooks.js
@@ -4,7 +4,11 @@
  * useEffect: useEffect is a Hook that allows you to manage side-effects like API calls, subscriptions, timers, mutations, and more
  * useRef: It returns a ref object with a .current property. The ref object is mutable. It is mainly used to access a child component imperatively.
  */
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { createContext, useCallback, useContext, useMemo, useRef, useState } from 'react';
+
+
+const ThemeContext = createContext();
+const ThemeUpdateContext = createContext();
 
 const Hooks = () => {
     const [count, setCount] = useState(0); // adding state to functional component
@@ -22,11 +26,46 @@ const Hooks = () => {
         return 100 + count;
     }, [count])
 
+    const theme = useTheme();
+    const updateTheme = useThemeUpdate();
+
     return <div>
         {count}
         <input ref={refText}>{memoValue}</input>
         <button onClick={callbackFn}>run call back</button>
+        <button onClick={updateTheme}> Update Theme</button>
+        <h3>{theme}</h3>
+        <ThemeProvider>
+            <ContextComp></ContextComp>
+        </ThemeProvider>
+            
         </div>
+}
+
+const ContextComp = () => {
+    const theme = useContext(ThemeContext);
+    console.log(theme)
+}
+
+
+const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState('dark');
+    const toggleTheme = () => {
+        if (theme === 'dark') {
+            setTheme('light')
+        } else {
+            setTheme('dark')
+        } 
+    }
+    return <ThemeContext.Provider value={theme}><ThemeUpdateContext.Provider value={toggleTheme}>{children}</ThemeUpdateContext.Provider></ThemeContext.Provider>
+}
+
+const useTheme = () => {
+    useContext(ThemeContext);
+}
+
+const useThemeUpdate = () => {
+    useContext(ThemeUpdateContext);
 }
 
 export default Hooks // use this component in react application
